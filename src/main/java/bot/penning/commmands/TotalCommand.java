ace73d43582638ab3570eac87d40b71c47cd4020
Katy <katyschifferer@gmail.com>
2022-12-29 13:10:20 -0600

    Update total

--- src/main/java/bot/penning/commmands/TotalCommand.java
+++ src/main/java/bot/penning/commmands/TotalCommand.java
@@ -1,5 +1,18 @@
 package bot.penning.commmands;
 
-public class TotalCommand {
+import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import reactor.core.publisher.Mono;
 
-}
+public class TotalCommand implements SlashCommand {
+	  @Override
+	    public String getName() {
+	        return "total";
+	    }
+
+	    @Override
+	    public Mono<Void> handle(ChatInputInteractionEvent event) {
+
+	        //Reply to the slash command, with the name the user supplied
+	    	return event.reply("Total added!");
+	    }
+	}
